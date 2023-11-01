70cae0cd920d72d457328fd8d05b89bfbaedb4c0
iamgeorgp <plus.george@yandex.ru>
2023-11-02 00:12:11 +0300

    example

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import city_class
 import func_dom_set
+import func_path_finder
 
-city = city_class.CityGrid(5, 6, obstructed_percentage=55)
+# Initialization of a city with specified parameters
+city = city_class.CityGrid(5, 7, obstructed_percentage=65)
+# Initialization of the graph of a city with towers with radius R
 city.init_graph(R=1)
+# Visualization of the city without towers
 city.vizualize_graph()
-city.vizualize_graph(towers=func_dom_set.find_minimum_dom_sets(city.graph))
+# Finding the minimum number of towers and their location in the city
+towers=func_dom_set.find_minimum_dom_sets(city.graph)
+# Visualization of tower placement in the city
+city.vizualize_graph(towers=towers)
+# Finding the most stable path between given towers for data transmission
+print('Shortest path: ', func_path_finder.path_between_towers(city, towers=towers))
